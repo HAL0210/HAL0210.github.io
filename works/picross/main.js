b830730054878cd0a5e15b9173eb933748305fca
HAL0210 <82308855+HAL0210@users.noreply.github.com>
2021-09-27 13:06:10 +0900

    Add files via upload

---
+++ works/picross/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+{
+  const N = 3;
+  for (var i = 0; i < N; i++) {
+    for (var j = 0; j < N; j++) {
+      
+    }
+  }
+
+  class Mass {
+    constructor() {
+      
+    }
+  }
+}
